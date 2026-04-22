d68261f60f0ac9134a2fee342f1886bd89f866e4
Amin Mirlohi <aminmirlohi14@gmail.com>
2026-04-21 22:17:16 -0400

    Added transaction system into app

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,9 @@
 const { sequelize } = require("../config/database");
 const User = require("./user.model");
+const Transaction = require("./transaction.model");
+
+User.hasMany(Transaction, { foreignKey: "userId", onDelete: "CASCADE" });
+Transaction.belongsTo(User, { foreignKey: "userId" });
 
 async function initModels() {
     await sequelize.sync();
@@ -8,5 +12,6 @@ async function initModels() {
 module.exports = {
     sequelize,
     User,
+    Transaction,
     initModels,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const healthRoutes = require("./health.routes");
 const authRoutes = require("./auth.routes");
+const transactionRoutes = require("./transaction.routes");
 
 const router = Router();
 
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
+router.use("/transactions", transactionRoutes);
 
 module.exports = router;
