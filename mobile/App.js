6c258b73768f047757cdd769ce4e2a530dfa62a7
Amin Mirlohi <aminmirlohi14@gmail.com>
2026-04-22 17:44:29 -0400

    Wrapped w Auth provider

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
+import { AuthProvider } from "./context/AuthContext";
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <AppNavigator />
-        </NavigationContainer>
+        <AuthProvider>
+            <NavigationContainer>
+                <AppNavigator />
+            </NavigationContainer>
+        </AuthProvider>
     );
 }
