c4c131550cf4228b223c110cacea63a8aea849b5
Amin Mirlohi <aminmirlohi14@gmail.com>
2026-04-22 00:28:07 -0400

    connect analytics route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const healthRoutes = require("./health.routes");
 const authRoutes = require("./auth.routes");
 const transactionRoutes = require("./transaction.routes");
+const analyticsRoutes = require("./analytics.routes");
 
 const router = Router();
 
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/transactions", transactionRoutes);
+router.use("/analytics", analyticsRoutes);
 
 module.exports = router;
